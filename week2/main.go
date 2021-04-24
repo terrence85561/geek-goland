fd937c243672e483aa9cd4068d16e5943d52d651
terrence85561 <tianyi4@ualberta.ca>
2021-04-24 10:46:10 +0800

    update

--- week2/main.go
+++ week2/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main()  {
-	
+import (
+	"database/sql"
+	"fmt"
+	"github.com/pkg/errors"
+)
+
+// queryFromDao mock query in DAO
+func queryFromDao() error {
+	return sql.ErrNoRows
+}
+
+func mockService() error {
+	err := queryFromDao()
+	if errors.Is(err, sql.ErrNoRows) {
+		return errors.Wrap(err, "some error info...")
+	}
+	return nil
+}
+
+func main() {
+	err := mockService()
+	fmt.Printf("%+v\n", err)
 }
